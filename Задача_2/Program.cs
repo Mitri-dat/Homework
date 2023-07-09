8a7b9cf0c9044a1cfa3f064c6ab7aaa1162a6a64
Mitri-dat <diamondds@mail.ru>
2023-07-09 13:12:05 +0700

    задача2

---
+++ Задача_2/Program.cs
@@ -0,0 +1,12 @@
+﻿        //4.Наибольшее из трех чисел
+Console.WriteLine("Введите число 1 : ");
+int m1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2 : ");
+int m2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 3 : ");
+int m3 = int.Parse(Console.ReadLine());
+int Mmax = 0;
+if (m1 > m2) Mmax = m1; else Mmax = m2;
+if (Mmax < m3) Mmax = m3;
+Console.WriteLine($"Наибольшее из заданных чисел: {Mmax}"); 
+
