f50d1576086c15ebaa8e8d647ad32a14d244fe5b
Mitri-dat <diamondds@mail.ru>
2023-07-09 13:13:36 +0700

    задача3

---
+++ Задача_3/Program.cs
@@ -0,0 +1,5 @@
+﻿    //6. Является ли число четным
+Console.WriteLine("Введите число : ");
+int number = int.Parse(Console.ReadLine());
+if (number%2 == 0)Console.WriteLine($"число {number} четное"); 
+else Console.WriteLine($"число {number} нечетное"); 
