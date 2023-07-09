6d20e8063098b681eb37dda23076504466a3e332
Mitri-dat <diamondds@mail.ru>
2023-07-09 13:14:32 +0700

    задача4

---
+++ Задача_4/Program.cs
@@ -0,0 +1,6 @@
+﻿      //8. На вход принимает число, на выходе все четные числа
+Console.WriteLine("Введите число : ");
+int number = int.Parse(Console.ReadLine());
+for (int i = 2; i <= number; i = i+2)
+   Console.Write($"{i},");  
+
