2d0f92e2e085f31f6a08a7a417f0799e850be32d
Mitri-dat <diamondds@mail.ru>
2023-07-09 13:05:03 +0700

    задача1

---
+++ Задача_1/Program.cs
@@ -0,0 +1,7 @@
+﻿    // 2. Большее из 2вух чисел
+Console.WriteLine("Введите число 1 : ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2 : ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2)Console.WriteLine($"{number1} > {number2}"); 
+else Console.WriteLine($"{number1} < {number2}");
